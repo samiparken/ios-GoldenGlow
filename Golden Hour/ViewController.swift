c269de3d661646f9ce9792768054458f5b446475
Han-Saem Park <samiparken@gmail.com>
2020-08-27 18:52:03 +0200

    Initial Commit

---
+++ Golden Hour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golden Hour
+//
+//  Created by Sam on 8/27/20.
+//  Copyright © 2020 Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
