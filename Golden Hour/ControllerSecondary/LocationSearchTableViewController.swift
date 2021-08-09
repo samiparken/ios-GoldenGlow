104b8e6945d497e602840f6c3523f9c5c89428d1
Han-Saem Park <samiparken@gmail.com>
2021-08-09 09:13:35 +0200

    LocationSearch: able to show search result

--- Golden Hour/ControllerSecondary/LocationSearchTableViewController.swift
+++ Golden Hour/ControllerSecondary/LocationSearchTableViewController.swift
@@ -1,9 +1,13 @@
 import UIKit
+import MapKit
 
 class LocationSearchTableViewController: UITableViewController {
     let myTabBar = TabBarController.singletonTabBar
 
     let searchController = UISearchController()
+    
+    var matchingItems: [MKMapItem] = []
+    var mapView: MKMapView? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,63 +32,17 @@ class LocationSearchTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return matchingItems.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell: LocationSearchCell = tableView.dequeueReusableCell(withIdentifier: "LocationSearchCell", for: indexPath) as! LocationSearchCell
 
-        // Configure the cell...
-
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.label?.text = selectedItem.name
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 
@@ -92,11 +50,21 @@ class LocationSearchTableViewController: UITableViewController {
 extension LocationSearchTableViewController: UISearchResultsUpdating {
     
     func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else {
+        guard let inputText = searchController.searchBar.text else {
             return
         }
+                
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = inputText
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
         
-        print(text)
     }
         
 }
---
+++ Golden Hour/ViewCell/LocationSearchCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class LocationSearchCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
